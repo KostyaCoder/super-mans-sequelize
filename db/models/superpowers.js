fe1a55fec02e00ef30fea335ad566dd19d5656e0
Kostya <forgit266@gmail.com>
2023-08-25 17:56:22 +0000

    Add model power

--- db/models/superpowers.js
+++ db/models/superpowers.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Superpowers extends Model {
     /**
@@ -9,15 +7,29 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Superman }) {
+      Superpowers.belongsToMany(Superman, {
+        through: "mans_to_powers",
+        foreignKey: "powerId",
+      });
     }
   }
-  Superpowers.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Superpowers',
-  });
+  Superpowers.init(
+    {
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Superpowers",
+      tableName: "superpowers",
+      underscored: true,
+    }
+  );
   return Superpowers;
 };
