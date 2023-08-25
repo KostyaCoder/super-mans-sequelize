48cb1e78b32ac2a9e023bbddaaf6ef3f4f90c8da
Kostya <forgit266@gmail.com>
2023-08-25 17:48:47 +0000

    Add model man

--- db/models/superman.js
+++ db/models/superman.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Superman extends Model {
     /**
@@ -9,18 +7,34 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Images, Superpowers }) {
+      Superman.hasMany(Images, { foreignKey: "manId" });
+      
+      Superman.belongsToMany(Superpowers, {
+        through: "mans_to_powers",
+        foreignKey: "manId",
+      });
     }
   }
-  Superman.init({
-    nickname: DataTypes.STRING,
-    description: DataTypes.STRING,
-    originDescription: DataTypes.TEXT,
-    catchPhrase: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Superman',
-  });
+  Superman.init(
+    {
+      nickname: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      realName: { type: DataTypes.STRING(100), field: "real_name" },
+      originDescription: { type: DataTypes.TEXT, field: "origin_description" },
+      catchPhrase: { type: DataTypes.STRING, field: "catch_phrase" },
+    },
+    {
+      sequelize,
+      modelName: "Superman",
+      tableName: "supermans",
+      underscored: true,
+    }
+  );
   return Superman;
 };
