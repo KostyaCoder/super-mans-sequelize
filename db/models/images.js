e1950fd454808e0e9ed29bd18f93574b82e833d2
Kostya <forgit266@gmail.com>
2023-08-25 18:06:40 +0000

    Add model image

--- db/models/images.js
+++ db/models/images.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Images extends Model {
     /**
@@ -9,15 +7,26 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Superman }) {
+      Images.belongsTo(Superman, { foreignKey: "manId" });
     }
   }
-  Images.init({
-    path: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Images',
-  });
+  Images.init(
+    {
+      path: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Images",
+      tableName: "images",
+      underscored: true,
+    }
+  );
   return Images;
 };
