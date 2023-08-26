acf30d525a7765cc304db400b9fd7a1c2a61e87d
Kostya <forgit266@gmail.com>
2023-08-26 10:17:54 +0000

    Add basic error

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = require('./routers');
+const basicError = require("./middlewares/error/basic");
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(basicError);
 
 module.exports = app;
