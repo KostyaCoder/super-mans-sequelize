856838cc7213e0c68bf25f0e14d1c2273e344f2b
Kostya <forgit266@gmail.com>
2023-08-26 08:56:42 +0000

    Add transaction

--- controllers/mans.controller.js
+++ controllers/mans.controller.js
@@ -1,21 +1,29 @@
 // const createHttpError = require("http-errors");
-const { Superman, Superpowers } = require("../db/models");
+const { Superman, Superpowers, sequelize } = require("../db/models");
 
 module.exports.createSuperman = async (req, resp, next) => {
+  const t = await sequelize.transaction();
+
   try {
     const {
       body,
       body: { powers },
     } = req;
 
-    const man = await Superman.create(body);
+    const man = await Superman.create(body, { transaction: t });
     for (const powerName of powers) {
-      const power = await Superpowers.create({name: powerName});
-      man.addSuperpowers(power);
+      const power = await Superpowers.create(
+        { name: powerName },
+        { transaction: t }
+      );
+      await man.addSuperpowers(power, { transaction: t });
     }
+
+    await t.commit();
 
     resp.status(201).send({ data: man });
   } catch (error) {
+    await t.rollback();
     next(error);
   }
 };
