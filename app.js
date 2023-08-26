985942f5972a894a09e1e7c912c8ce627133be11
Kostya <forgit266@gmail.com>
2023-08-26 14:02:47 +0000

    Add static

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = require('./routers');
 const basicError = require("./middlewares/error/basic");
+const constants = require('./constants');
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(express.static(constants.FILE_PATH));
 app.use(basicError);
 
 module.exports = app;
