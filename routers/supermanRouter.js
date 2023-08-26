06630dc605365f00cd1a782ccbb81e1bd8b0550e
Kostya <forgit266@gmail.com>
2023-08-26 14:45:44 +0000

    Add multer route

--- routers/supermanRouter.js
+++ routers/supermanRouter.js
@@ -2,13 +2,26 @@ const supermanRouter = require("express").Router();
 const supermanController = require("../controllers/mans.controller");
 const { findMan } = require("../middlewares/findMan.mv");
 const powerRouter = require("./powerRouter");
+const { uploadeFile } = require("../middlewares/uploadeFile.mv");
+const imageRouter = require("./imageRouter");
 
-supermanRouter.post("/", supermanController.createSuperman);
+supermanRouter.post(
+  "/",
+  uploadeFile.single("photo"),
+  supermanController.createSuperman
+);
 supermanRouter.get("/", supermanController.getSupermans);
 supermanRouter.get("/:manId", findMan, supermanController.getSuperman);
 supermanRouter.put("/:manId", findMan, supermanController.updateSuperman);
 supermanRouter.delete("/:manId", findMan, supermanController.deleteSuperman);
 
 supermanRouter.use("/:manId/powers", findMan, powerRouter);
+
+supermanRouter.use(
+  "/:manId/images",
+  findMan,
+  uploadeFile.single("photo"),
+  imageRouter
+);
 
 module.exports = supermanRouter;
