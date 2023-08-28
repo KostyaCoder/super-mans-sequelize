35d7e3cb6d1b4b852a8c5bd545e2e647a10019c4
Kostya <forgit266@gmail.com>
2023-08-28 12:16:52 +0000

    Add update power

--- controllers/powers.controller.js
+++ controllers/powers.controller.js
@@ -1,4 +1,4 @@
-const { sequelize } = require("../db/models");
+const { sequelize, Superpowers } = require("../db/models");
 
 module.exports.createSuperpower = async (req, resp, next) => {
   const t = await sequelize.transaction();
@@ -13,6 +13,24 @@ module.exports.createSuperpower = async (req, resp, next) => {
     resp.status(201).send({ data: power });
   } catch (error) {
     await t.rollback();
+    next(error);
+  }
+};
+
+module.exports.updateSuperpower = async (req, resp, next) => {
+  try {
+    const {
+      body,
+      params: { powerId },
+    } = req;
+
+    const [countRows, [superpower]] = await Superpowers.update(body, {
+      where: { id: powerId },
+      returning: true,
+    });
+
+    resp.status(200).send({ data: superpower });
+  } catch (error) {
     next(error);
   }
 };
--- routers/powerRouter.js
+++ routers/powerRouter.js
@@ -2,5 +2,6 @@ const powerRouter = require("express").Router();
 const powerController = require("../controllers/powers.controller");
 
 powerRouter.post("/", powerController.createSuperpower);
+powerRouter.put("/:powerId", powerController.updateSuperpower);
 
 module.exports = powerRouter;
